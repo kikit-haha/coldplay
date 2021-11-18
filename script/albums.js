ddd75318bff37ffdc724187179c09b0ffeeb5e98
kikit-haha <61496053+kikit-haha@users.noreply.github.com>
2021-11-18 18:59:42 +0900

    fix to ger album api

--- script/albums.js
+++ script/albums.js
@@ -1,16 +1,13 @@
-$(document).ready(function () {
-  const settings = {
-    async: true,
-    crossDomain: true,
-    url: "https://theaudiodb.p.rapidapi.com/album.php?i=111239",
-    method: "GET",
-    headers: {
-      "x-rapidapi-host": "theaudiodb.p.rapidapi.com",
-      "x-rapidapi-key": "8e5c5dd52bmsh23e34ee18ef0108p1a5fcbjsn864615166e2d",
-    },
-  };
-
-  $.ajax(settings).done(function (response) {
+fetch("https://theaudiodb.p.rapidapi.com/album.php?i=111239", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "theaudiodb.p.rapidapi.com",
+    "x-rapidapi-key": "8e5c5dd52bmsh23e34ee18ef0108p1a5fcbjsn864615166e2d",
+  },
+})
+  .then((response) => {
     console.log(response);
+  })
+  .catch((err) => {
+    console.error(err);
   });
-});
